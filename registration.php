e794fa29092b8c9c06b3795e48a354609515ff34
Amandeep Mann <amandeepji13@gmail.com>
2022-02-28 16:44:42 +0530

    submiting files

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © JoynBe. All rights reserved.
+ * See COPYING.txt for license details.
+ * http://www.joyn.be | support@joyn.be
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Joyn_JoynBe',
+    __DIR__
+);
